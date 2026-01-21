410b696a3f1c2dea6f9c43853995c6d996c54ac4
wajhi2004-commits <wajhi2004@gmail.com>
2026-01-21 16:08:36 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,27 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList();
 
-        // Insert at beginning
+
         dll.insertAtBeginning(10);
         dll.insertAtBeginning(20);
 
-        // Insert at end
+       
         dll.insertAtEnd(30);
         dll.insertAtEnd(40);
         dll.insertAtEnd(5);
         dll.insertAtEnd(50);
 
-        dll.displayForward();
+        dll.displayForward();    
         dll.displayBackward();
 
-        dll.deleteFirst();
+        dll.deleteFirst();    //Complexity =  O(1)
         dll.deleteLast();
 
-        dll.deleteByValue(20);
-        dll.deleteBeforeValue(30);
+        dll.deleteByValue(20);        //Complexity = O(n)
+        dll.deleteBeforeValue(30);    //Complexity= O(n)
         dll.deleteAfterValue(20);
 
         dll.displayForward();
-        dll.findMinMax();
+        dll.findMinMax();    // Complexity= O(n)
     }
 }
