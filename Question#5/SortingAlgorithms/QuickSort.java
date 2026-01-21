1e5cc89daf2ddc714a7d639968d0314ee758996c
wajhi2004-commits <wajhi2004@gmail.com>
2026-01-21 18:11:13 +0500

    Q#5

---
+++ Question#5/SortingAlgorithms/QuickSort.java
@@ -0,0 +1,37 @@
+
+    //  QUICK SORT
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    // Print array
+    static void printArray(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
